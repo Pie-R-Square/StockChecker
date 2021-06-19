37d5e8dec3894749a6879ae54a1d51a126be2958
Pie-R-Square <85381917+Pie-R-Square@users.noreply.github.com>
2021-06-19 11:56:43 +0700

    Add: Stock overview

--- Stock_Checker.py
+++ Stock_Checker.py
@@ -3,19 +3,51 @@ from tkinter import ttk, messagebox
 import csv
 from datetime import datetime
 
-GUI = Tk()
-GUI.title("Stock Checker")
+root = Tk()
+root.title("Stock Checker")
 
-w = 1200
+
+w = 600
 h = 650
 
-ws = GUI.winfo_screenwidth() #screen width
-hs = GUI.winfo_screenheight() #screen height
+ws = root.winfo_screenwidth() #screen width
+hs = root.winfo_screenheight() #screen height
 
 
 x = (ws/2) - (w/2)
 y = (hs/2) - (h/2)
 
-GUI.geometry(f'{w}x{h}+{x:.0f}+{y:.0f}')
+root.geometry(f'{w}x{h}+{x:.0f}+{y:.0f}')
 
-GUI.mainloop()
+F1 = LabelFrame(root, text="Input", font=16, padx=20, pady=20)
+F1.pack(pady=30)
+F2 = LabelFrame(root, text="Output", font=16, padx=20, pady=20)
+F2.pack()
+F3 = LabelFrame(root, text="Output", font=16, padx=20, pady=20)
+F3.pack(pady=30)
+
+##### Stock oveerview #####
+header = ["Original", "Seaweed", "Tom Yum", "Herbal"]
+input = ttk.Treeview(F1, columns=header, show='headings', height=1)
+input.pack()
+for h in header:
+	input.heading(h, text=h)
+
+output = ttk.Treeview(F2, columns=header, show='headings',height=1)
+output.pack()
+for h in header:
+	output.heading(h, text=h)
+
+balance = ttk.Treeview(F3, columns=header, show='headings',height=1)
+balance.pack()
+for h in header:
+	balance.heading(h, text=h)
+
+headerwidth = [80, 80, 80, 80]
+for h, w in zip(header, headerwidth):
+	input.column(h, width=w)
+	output.column(h, width=w)
+	balance.column(h, width=w)
+##### End #####
+
+root.mainloop()
